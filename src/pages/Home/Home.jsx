1af88d5c7dd7b2c9daf393ad9d697dc112d76071
icaroboaventura <boaventura@outlook.de>
2024-03-15 16:56:34 -0300

    update

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -25,32 +25,34 @@ const Home = () => {
 
   return (
     <>
-      <section className="flex items-center justify-center gap-4 py-7">
-        <button className="bg-gray-500 rounded-xl p-2" onClick={() => setFilterOption("asc")}>
+      <section className="flex items-center justify-center gap-4 py-7 font-gillSans">
+        <button className="bg-yellow-400 text-yellow-100 rounded-xl p-2" onClick={() => setFilterOption("asc")}>
           Date Ascending
         </button>
-        <button className="bg-gray-500 rounded-xl p-2" onClick={() => setFilterOption("desc")}>
+        <button className="bg-yellow-400 text-yellow-100 rounded-xl p-2" onClick={() => setFilterOption("desc")}>
           Date Desending
         </button>
-        <button className="bg-gray-500 rounded-xl p-2" onClick={() => setFilterOption("rate")}>
+        <button className="bg-yellow-400 text-yellow-100 rounded-xl p-2" onClick={() => setFilterOption("rate")}>
           Best Rate
         </button>
-        <button className="bg-gray-500 rounded-xl p-2" onClick={() => setFilterOption("az")}>
+        <button className="bg-yellow-400 text-yellow-100 rounded-xl p-2" onClick={() => setFilterOption("az")}>
           A-Z
         </button>
-        <button className="bg-gray-500 rounded-xl p-2" onClick={() => setFilterOption("za")}>
+        <button className="bg-yellow-400 text-yellow-100 rounded-xl p-2" onClick={() => setFilterOption("za")}>
           Z-A
         </button>
       </section>
-      <section className="flex flex-wrap items-center justify-center gap-10">
+      <section className="flex flex-wrap items-center justify-center gap-10 font-gillSans">
         {movies.map((movie, index) => (
           <div key={index} className="bg-slate-500 w-52 h-72 p-3 flex flex-col text-center rounded-xl">
-            <h1>{movie.title}</h1>
-            <p>{movie.year}</p>
-            <p>{movie.director}</p>
-            <p>{movie.duration}</p>
+            <h1 className="text-xl font-bold text-yellow-400">{movie.title}</h1>
+            <p className="text-yellow-100">{movie.year}</p>
+            <p className="text-yellow-100">{movie.director}</p>
+            <p className="text-yellow-100">{movie.duration}</p>
             {movie.genre.map((singleGenre, index) => (
-              <p key={index}>{singleGenre}</p>
+              <p className="text-yellow-100" key={index}>
+                {singleGenre}
+              </p>
             ))}
             <StarRating rating={movie.rate} />
           </div>
