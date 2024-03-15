828859c1237484e90fa0a3cb1060baaae42662c1
icaroboaventura <boaventura@outlook.de>
2024-03-15 17:46:05 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,11 @@ import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Home from "./pages/Home/Home";
 function App() {
   return (
-    <div className="bg-yellow-100">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -25,14 +25,14 @@ const Home = () => {
   }, [filterOption]);
 
   return (
-    <>
+    <div className="bg-yellow-100 min-h-svh">
       <section>
         <label className="flex items-center justify-center pt-10">
           <input className="font-gillSans rounded-xl p-2 drop-shadow-md" type="text" name="" id="search" placeholder="Search Movie" onChange={(event) => setInput(event.target.value)} value={input} />
         </label>
       </section>
       <section className="flex items-center justify-center gap-4 py-7 font-gillSans">
-        <button className="bg-yellow-400 text-yellow-100 rounded-xl p-2 focus:bg-yellow-900 focus:text-yellow-400 " onClick={() => setFilterOption("asc")}>
+        <button className="bg-yellow-400 text-yellow-100 rounded-xl p-2 focus:bg-yellow-900 focus:text-yellow-400 drop-shadow-md" onClick={() => setFilterOption("asc")}>
           Date Ascending
         </button>
         <button className="bg-yellow-400 text-yellow-100 rounded-xl p-2 focus:bg-yellow-900 focus:text-yellow-400 " onClick={() => setFilterOption("desc")}>
@@ -53,7 +53,7 @@ const Home = () => {
           <section className="flex flex-wrap items-center justify-center gap-10 font-gillSans">
             {movies.map((movie, index) =>
               movie.title.toLocaleLowerCase().includes(input.toLocaleLowerCase()) ? (
-                <div key={index} className="bg-yellow-900 w-52 h-72 p-3 flex flex-col text-center rounded-xl">
+                <div key={index} className="bg-yellow-900 w-52 h-72 p-3 flex flex-col text-center rounded-xl drop-shadow-xl">
                   <h1 className="text-xl font-bold text-yellow-400">{movie.title}</h1>
                   <p className="text-yellow-100">{movie.year}</p>
                   <p className="text-yellow-100">{movie.director}</p>
@@ -89,7 +89,7 @@ const Home = () => {
           </section>
         )}
       </div>
-    </>
+    </div>
   );
 };
 
