8c8c73b42e65cdf2e9d99615d83da9bc70d99b87
icaroboaventura <boaventura@outlook.de>
2024-03-15 17:34:29 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,13 @@ import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Home from "./pages/Home/Home";
 function App() {
   return (
-    <>
+    <div className="bg-yellow-100">
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
       </BrowserRouter>
-    </>
+    </div>
   );
 }
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,6 +5,7 @@ import StarRating from "../../components/StarRating/StarRating";
 const Home = () => {
   const [movies, setMovies] = useState(moviesData);
   const [filterOption, setFilterOption] = useState("");
+  const [input, setInput] = useState("");
 
   useEffect(() => {
     movies.sort((a, b) => {
@@ -25,39 +26,69 @@ const Home = () => {
 
   return (
     <>
+      <section>
+        <label className="flex items-center justify-center pt-10">
+          <input className="font-gillSans rounded-xl p-2 drop-shadow-md" type="text" name="" id="search" placeholder="Search Movie" onChange={(event) => setInput(event.target.value)} value={input} />
+        </label>
+      </section>
       <section className="flex items-center justify-center gap-4 py-7 font-gillSans">
-        <button className="bg-yellow-400 text-yellow-100 rounded-xl p-2" onClick={() => setFilterOption("asc")}>
+        <button className="bg-yellow-400 text-yellow-100 rounded-xl p-2 focus:bg-yellow-900 focus:text-yellow-400 " onClick={() => setFilterOption("asc")}>
           Date Ascending
         </button>
-        <button className="bg-yellow-400 text-yellow-100 rounded-xl p-2" onClick={() => setFilterOption("desc")}>
+        <button className="bg-yellow-400 text-yellow-100 rounded-xl p-2 focus:bg-yellow-900 focus:text-yellow-400 " onClick={() => setFilterOption("desc")}>
           Date Desending
         </button>
-        <button className="bg-yellow-400 text-yellow-100 rounded-xl p-2" onClick={() => setFilterOption("rate")}>
+        <button className="bg-yellow-400 text-yellow-100 rounded-xl p-2 focus:bg-yellow-900 focus:text-yellow-400 " onClick={() => setFilterOption("rate")}>
           Best Rate
         </button>
-        <button className="bg-yellow-400 text-yellow-100 rounded-xl p-2" onClick={() => setFilterOption("az")}>
+        <button className="bg-yellow-400 text-yellow-100 rounded-xl p-2 focus:bg-yellow-900 focus:text-yellow-400 " onClick={() => setFilterOption("az")}>
           A-Z
         </button>
-        <button className="bg-yellow-400 text-yellow-100 rounded-xl p-2" onClick={() => setFilterOption("za")}>
+        <button className="bg-yellow-400 text-yellow-100 rounded-xl p-2 focus:bg-yellow-900 focus:text-yellow-400 " onClick={() => setFilterOption("za")}>
           Z-A
         </button>
       </section>
-      <section className="flex flex-wrap items-center justify-center gap-10 font-gillSans">
-        {movies.map((movie, index) => (
-          <div key={index} className="bg-slate-500 w-52 h-72 p-3 flex flex-col text-center rounded-xl">
-            <h1 className="text-xl font-bold text-yellow-400">{movie.title}</h1>
-            <p className="text-yellow-100">{movie.year}</p>
-            <p className="text-yellow-100">{movie.director}</p>
-            <p className="text-yellow-100">{movie.duration}</p>
-            {movie.genre.map((singleGenre, index) => (
-              <p className="text-yellow-100" key={index}>
-                {singleGenre}
-              </p>
+      <div>
+        {input.length > 0 ? (
+          <section className="flex flex-wrap items-center justify-center gap-10 font-gillSans">
+            {movies.map((movie, index) =>
+              movie.title.toLocaleLowerCase().includes(input.toLocaleLowerCase()) ? (
+                <div key={index} className="bg-yellow-900 w-52 h-72 p-3 flex flex-col text-center rounded-xl">
+                  <h1 className="text-xl font-bold text-yellow-400">{movie.title}</h1>
+                  <p className="text-yellow-100">{movie.year}</p>
+                  <p className="text-yellow-100">{movie.director}</p>
+                  <p className="text-yellow-100">{movie.duration}</p>
+                  {movie.genre.map((singleGenre, index) => (
+                    <p className="text-yellow-100" key={index}>
+                      {singleGenre}
+                    </p>
+                  ))}
+                  <StarRating rating={movie.rate} />
+                </div>
+              ) : (
+                ""
+              )
+            )}
+          </section>
+        ) : (
+          <section className="flex flex-wrap items-center justify-center gap-10 font-gillSans">
+            {movies.map((movie, index) => (
+              <div key={index} className="bg-yellow-900 w-52 h-72 p-3 flex flex-col text-center rounded-xl drop-shadow-xl">
+                <h1 className="text-xl font-bold text-yellow-400">{movie.title}</h1>
+                <p className="text-yellow-100">{movie.year}</p>
+                <p className="text-yellow-100">{movie.director}</p>
+                <p className="text-yellow-100">{movie.duration}</p>
+                {movie.genre.map((singleGenre, index) => (
+                  <p className="text-yellow-100" key={index}>
+                    {singleGenre}
+                  </p>
+                ))}
+                <StarRating rating={movie.rate} />
+              </div>
             ))}
-            <StarRating rating={movie.rate} />
-          </div>
-        ))}
-      </section>
+          </section>
+        )}
+      </div>
     </>
   );
 };
